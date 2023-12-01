0989db7832adc01f37b57c4815f874a330eaa0ca
Tim Quattrochi <timq82@gmail.com>
2023-11-30 21:39:38 -0500

    Add JWT constants to config

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,9 @@
 const PORT = process.env.PORT;
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 
 module.exports = {
   PORT,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
 };
