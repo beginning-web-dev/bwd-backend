cbd57580b82dcda7a62d64909d575508a2293063
Tim Quattrochi <timq82@gmail.com>
2023-11-30 22:04:04 -0500

    Update auth route path

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use("/auth", authRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
