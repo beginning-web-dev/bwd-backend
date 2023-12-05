bb248b8379ce9d6cff57851f2680ff53d023caea
Tim Quattrochi <timq82@gmail.com>
2023-12-04 22:50:37 -0500

    Add env example and login route comment

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,9 @@ const authRouter = express.Router();
 
 const authController = require("../controllers/auth.controller");
 
+/**
+http://localhost:3001/api/v1/auth/login
+ */
 authRouter.post("/login", authController.login);
 
 module.exports = authRouter;
